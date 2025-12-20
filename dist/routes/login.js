0f441ebff6e9a3ee7c940ca77807168be899eaf8
yusufsw1 <yusufsw390@gmail.com>
2025-12-20 10:53:34 +0700

    Final Deploy

--- dist/routes/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//login.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../controller/auth");
-const router = (0, express_1.Router)();
-router.post("/google-verify", auth_1.googleVerify);
-exports.default = router;
